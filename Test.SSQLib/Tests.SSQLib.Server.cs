c48d8741ef2a9d503cff6d699c9f63f9a7d8e93e
Lee Walker <lee.walker@commontime.com>
2015-05-12 15:27:20 +0100

    Added basic test project and basic test stuff

---
+++ Test.SSQLib/Tests.SSQLib.Server.cs
@@ -0,0 +1,14 @@
+﻿
+using Xunit;
+
+namespace Test.SSQLib
+{
+    public class Tests
+    {
+        [Fact]
+        public void MockTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
