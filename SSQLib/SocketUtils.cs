ae669d6b0d5a703fe6aad7c0917662ad4756c885
benbryant0 <benbryant0@gmail.com>
2017-07-30 13:18:28 +0200

    Remove unnecessary using statements

--- SSQLib/SocketUtils.cs
+++ SSQLib/SocketUtils.cs
@@ -15,10 +15,6 @@
  *   along with SSQLib.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
